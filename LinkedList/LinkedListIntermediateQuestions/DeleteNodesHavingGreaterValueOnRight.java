140d3d1248581e47d1ecdc6e19a348ecd51d0c10
Pradip <pradipsahoo7722@gmail.com>
2021-07-20 12:24:25 +0530

    Add new file

---
+++ LinkedList/LinkedListIntermediateQuestions/DeleteNodesHavingGreaterValueOnRight.java
@@ -0,0 +1,144 @@
+/**
+ * Delete nodes which having greater value on right side.
+ */
+package LinkedList.LinkedListIntermediateQuestions;
+
+import java.util.Scanner;
+
+public class DeleteNodesHavingGreaterValueOnRight {
+
+    static Node headNode;
+    static Node tempNode;
+
+    public class Node {
+
+        int data;
+        Node nextNode;
+
+        public Node(int data) {
+            this.data = data;
+            nextNode = null;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        // Create Scanner class object to take user input
+        Scanner sc = new Scanner(System.in);
+
+        // Ask user to enter the length of the linked list
+        System.out.println("Enter the length of the linked list : ");
+        int length = sc.nextInt();
+
+        // Create object of the class
+        DeleteNodesHavingGreaterValueOnRight dOnRight = new DeleteNodesHavingGreaterValueOnRight();
+
+        // Ask user to enter the elements of the list
+        System.out.println("Enter the elements of the list : ");
+        for (int i = 0; i < length; i++) {
+            int val = sc.nextInt();
+            Node hNode = dOnRight.new Node(val);
+            createNewNode(hNode);
+        }
+
+        sc.close();
+
+        // Method call to print the general list
+        System.out.print("The general list is : ");
+        printList(headNode);
+
+        // Method call to delete nodes from right
+        headNode = deleteNodeFromRight(headNode);
+
+        // Method call to print the final list
+        System.out.print("The final list is : ");
+        printList(headNode);
+    }
+
+    // Method to delete nodes which have greater value on right
+    private static Node deleteNodeFromRight(Node head) {
+
+        // Reverse the list
+        Node curr = head;
+        Node prev = null;
+        Node next;
+
+        while (curr != null) {
+            next = curr.nextNode;
+            curr.nextNode = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        head = prev;
+
+        /**
+         * Computing and deleting the nodes which have larger nodes on the left
+         */
+        Node current = head;
+        Node maxNode = head;
+
+        while (current != null && current.nextNode != null) {
+
+            /**
+             * If this condition is true then we store the next of next value of current in
+             * the position of next value of current
+             */
+            if (current.nextNode.data < maxNode.data) {
+                current.nextNode = current.nextNode.nextNode;
+            }
+            /**
+             * else we update the recent current value update the maxNode by recent current
+             * value
+             */
+            else {
+                current = current.nextNode;
+                maxNode = current;
+            }
+        }
+
+        
+        // reversing list again to get required result
+        curr = head;
+        prev = null;
+
+        while (curr != null) {
+            next = curr.nextNode;
+            curr.nextNode = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        return prev;
+    }
+
+    // Method to print the list
+    private static void printList(Node head) {
+
+        if (head == null) {
+            System.out.println("No list found");
+        }
+
+        while (head != null) {
+            System.out.print(head.data + "->");
+            head = head.nextNode;
+        }
+        System.out.println("null");
+    }
+
+    // Method to add new elements in the list
+    private static void createNewNode(Node node) {
+
+        if (headNode == null) {
+            headNode = node;
+            return;
+        }
+
+        tempNode = headNode;
+
+        while (tempNode.nextNode != null) {
+            tempNode = tempNode.nextNode;
+        }
+        tempNode.nextNode = node;
+    }
+}
