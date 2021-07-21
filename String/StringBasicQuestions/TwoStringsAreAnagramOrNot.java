b6769de3d6dd11af0a7896d784539b9458799a28
Pradip <pradipsahoo7722@gmail.com>
2021-07-21 22:08:17 +0530

    Improve method checkAnagram

--- String/StringBasicQuestions/TwoStringsAreAnagramOrNot.java
+++ String/StringBasicQuestions/TwoStringsAreAnagramOrNot.java
@@ -7,8 +7,6 @@ import java.util.Scanner;
 
 public class TwoStringsAreAnagramOrNot {
 
-    static int No_Of_Char = 256;
-
     public static void main(String[] args) {
 
         // Create Scanner class to take user input
@@ -18,20 +16,14 @@ public class TwoStringsAreAnagramOrNot {
         System.out.println("Enter the first string : ");
         String str1 = sc.nextLine();
 
-        // Convert the string into array
-        char ch1[] = str1.toCharArray();
-
         // Ask user to enter the second string
         System.out.println("Enter the second string : ");
         String str2 = sc.nextLine();
 
         sc.close();
 
-        // Convert the string into array
-        char ch2[] = str2.toCharArray();
-
         // Method call to check the string is anagram or not
-        boolean isAnagram = checkAnagram(ch1, ch2);
+        boolean isAnagram = checkAnagram(str1, str2);
 
         // print the result
         if (isAnagram) {
@@ -42,32 +34,30 @@ public class TwoStringsAreAnagramOrNot {
     }
 
     // Method to check the string is anagram or not
-    private static boolean checkAnagram(char[] ch1, char[] ch2) {
-
-        // Create a count array and initialize all values as 0.
-        int count[] = new int[No_Of_Char];
-        int i;
-
-        /**
-         * For each character in input string, increment count in the corresponding
-         * count array
-         */
-        for (i = 0; i < ch1.length; i++) {
-            count[ch1[i] - 'a']++;
-            count[ch2[i] - 'a']--;
-        }
+    private static boolean checkAnagram(String str1, String str2) {
 
         /**
          * If the length of the two strings are not same then this condition returns
          * false
          */
-        if (ch1.length != ch2.length) {
+        if (str1.length() != str2.length()) {
             return false;
         }
 
-        // If there is any non zero value in count array
-        for (i = 0; i < No_Of_Char; i++) {
-            if (count[i] != 0) {
+        // Create a count array and initialize all values as 0.
+        int count[] = new int[256];
+
+        /**
+         * For each character in input string, increment count in the corresponding
+         * count array
+         */
+        for (int i = 0; i < str1.length(); i++) {
+            count[str1.charAt(i) - 'a']++;
+            count[str2.charAt(i) - 'a']--;
+        }
+
+        for (int res : count) {
+            if (res != 0) {
                 return false;
             }
         }
