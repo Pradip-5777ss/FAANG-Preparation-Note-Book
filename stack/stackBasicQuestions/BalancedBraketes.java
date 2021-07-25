a38a6bebd03ace434e2ea3cdf443d01aa6be323a
Pradip <pradipsahoo7722@gmail.com>
2021-07-26 02:24:24 +0530

    Minor modification

--- stack/stackBasicQuestions/BalancedBraketes.java
+++ stack/stackBasicQuestions/BalancedBraketes.java
@@ -1,20 +1,26 @@
 /**
- * Check the expression	has valid of Balanced parenthesis or not
- * @author Pradip
+ * Check the expression	has valid of Balanced parenthesis or not.
  */
 package stack.stackBasicQuestions;
 
 import java.util.ArrayDeque;
 import java.util.Deque;
+import java.util.Scanner;
 
 public class BalancedBraketes {
 
 	public static void main(String[] args) {
 
-		//	Declare a character stack
-		String brackets = "({[]})";
+		// Create Scanner class object to take user input
+		Scanner sc = new Scanner(System.in);
 
-		//	Function Call
+		// Ask user to enter the brackets
+		System.out.print("Enter the brackets : ");
+		String brackets = sc.nextLine();
+
+		sc.close();
+
+		// Method Call to check that the brackets are balanced or not
 		if (bracketBalanced(brackets)) {
 			System.out.println("Balanced");
 		} else {
@@ -23,27 +29,26 @@ public class BalancedBraketes {
 	}
 
 	/**
-	 * Function to check if brackets are balanced
-	 * It take @param bracket
-	 * @return
+	 * Method to check that brackets are balanced or not. It take @param bracket and
+	 * 
+	 * @return either true or false
 	 */
 	static boolean bracketBalanced(String bracket) {
 
-		//	Using ArrayDeque is faster than using Stack Class
+		// Using ArrayDeque is faster than using Stack Class
 		Deque<Character> stackDeque = new ArrayDeque<Character>();
 
-		//	Traversing the Expression
+		// Traversing the Expression
 		for (int i = 0; i < bracket.length(); i++) {
-			
+
 			/**
-			 * charAt returns the char value at the specified index of the string.
-			 * We store the char value in the variable.
+			 * charAt returns the char value at the specified index of the string. We store
+			 * the char value in the variable.
 			 */
 			char x = bracket.charAt(i);
-			
+
 			/**
-			 * If the current character is a starting bracket
-			 * then push it into the stack
+			 * If the current character is a starting bracket then push it into the stack
 			 */
 			if (x == '(' || x == '{' || x == '[') {
 				stackDeque.push(x);
@@ -51,48 +56,44 @@ public class BalancedBraketes {
 			}
 
 			/**
-			 * If current character is not opening bracket, 
-			 * then it must be closing. 
-			 * So stack cannot be empty at this point.
+			 * If current character is not opening bracket, then it must be closing. So
+			 * stack cannot be empty at this point.
 			 */
 			if (stackDeque.isEmpty()) {
 				return false;
 			}
-			
+
 			/**
-			 * Declare a char variable
-			 * If the current character is a closing bracket then
-			 * it pop from stack and if the popped character is matching
-			 * with the starting bracket then the brackets are balanced
-			 * else the brackets are not balanced. 
+			 * Declare a char variable If the current character is a closing bracket then it
+			 * pop from stack and if the popped character is matching with the starting
+			 * bracket then the brackets are balanced else the brackets are not balanced.
 			 */
 			char check;
 			switch (x) {
-			case ')':
-				check = stackDeque.pop();
-				if (check == '{' || check == '[') {
-					return false;
-				}
-				break;
+				case ')':
+					check = stackDeque.pop();
+					if (check == '{' || check == '[') {
+						return false;
+					}
+					break;
 
-			case '}':
-				check = stackDeque.pop();
-				if (check == '(' || check == '[') {
-					return false;
-				}
-				break;
+				case '}':
+					check = stackDeque.pop();
+					if (check == '(' || check == '[') {
+						return false;
+					}
+					break;
 
-			case ']':
-				check = stackDeque.pop();
-				if (check == '(' || check == '{') {
-					return false;
-				}
-				break;
+				case ']':
+					check = stackDeque.pop();
+					if (check == '(' || check == '{') {
+						return false;
+					}
+					break;
 			}
-
 		}
 
-		//	Check Empty stack
+		// Check Empty stack
 		return (stackDeque.isEmpty());
 	}
 }
