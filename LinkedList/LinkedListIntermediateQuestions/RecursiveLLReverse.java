9c9741b1de8e7b65447c4b980a62e22fb3a183cf
Pradip <Pradipsahoo7722@gmail.com>
2021-09-13 21:59:54 +0530

    Add Question & modify comments

--- LinkedList/LinkedListIntermediateQuestions/RecursiveLLReverse.java
+++ LinkedList/LinkedListIntermediateQuestions/RecursiveLLReverse.java
@@ -1,5 +1,13 @@
 /**
+ * Reverse Linked List Recursively.
  * Write a program to reverse a linked list Recursively.
+ * 
+ * Example:
+ *      Input : 1->2->3->4->NULL
+ *      Output : 4->3->2->1->NULL
+ * 
+ * Time Complexity : O(n)
+ * Space Complexity : O(1)
  */
 package LinkedList.LinkedListIntermediateQuestions;
 
@@ -10,30 +18,31 @@ public class RecursiveLLReverse {
     static Node headNode;
     static Node tempNode;
 
+    // Node class for creating Linked List
     public class Node {
-    
+
         int data;
         Node nextNode;
 
         public Node(int data) {
             this.data = data;
             nextNode = null;
-        }        
+        }
     }
 
     public static void main(String[] args) {
-        
-        //  Create Scanner class object to take user input
+
+        // Create Scanner class object to take user input
         Scanner sc = new Scanner(System.in);
 
-        //  Ask user to enter the length of the linked list
-        System.out.println("Enter the length of the linked list : ");
+        // Ask user to enter the length of the linked list
+        System.out.print("Enter the length of the linked list : ");
         int length = sc.nextInt();
 
-        //  create object of the class
+        // create object of the class
         RecursiveLLReverse llReverse = new RecursiveLLReverse();
 
-        //  Ask user to enter the elements of list
+        // Ask user to enter the elements of list
         System.out.println("Enter the elements of the list");
         for (int i = 0; i < length; i++) {
             int val = sc.nextInt();
@@ -42,64 +51,64 @@ public class RecursiveLLReverse {
         }
         sc.close();
 
-        //  Print the general list
+        // Print the general list
         System.out.print("The general list is : ");
         printList();
 
-        //  Method call to reverse the list
+        // Method call to reverse the list recursively
         headNode = reverseListRecursively(headNode);
 
-        //  Print the reverse list
+        // Print the reverse list
         System.out.print("The reverse list is : ");
         printList();
     }
 
-    //  Method to revere the list
-    private static Node reverseListRecursively(Node node) {
-
-        /**
-         * base case
-         * It returns the last node
-         */  
-        if (node == null || node.nextNode == null) {
-            return node;
-        }
- 
-        // recursive call
-        Node newHead = reverseListRecursively(node.nextNode);
-
-        //  Reverse the link
-        node.nextNode.nextNode = node;
-        node.nextNode = null;
-
-        //  Return new head
-        return newHead;
-    }
-
-    //  Method to print the linked list
-    private static void printList() {
-        tempNode = headNode;
-        
-        while (tempNode != null) {
-            System.out.print(tempNode.data + " ");
-            tempNode = tempNode.nextNode;
-        }
-        System.out.println();
-    }
-
-    //  Method to add new element in list
+    // Method to add new element in list
     private static void createNewNode(Node node) {
 
+        // First time when the List is empty then create the headNode
         if (headNode == null) {
             headNode = node;
             return;
         }
 
+        // Make the headNode as tempNode
         tempNode = headNode;
 
+        // Add other nodes in the list
         while (tempNode.nextNode != null) {
             tempNode = tempNode.nextNode;
         }
         tempNode.nextNode = node;
+    }
+
+    // Method to reverse the list recursively
+    private static Node reverseListRecursively(Node head) {
+
+        // base case
+        if (head == null || head.nextNode == null) {
+            return head;
+        }
+
+        // recursive call
+        Node newHead = reverseListRecursively(head.nextNode);
+
+        // Reverse the link
+        head.nextNode.nextNode = head;
+        head.nextNode = null;
+
+        // Return new head
+        return newHead;
+    }
+
+    // Method to print the Linked List
+    private static void printList() {
+        tempNode = headNode;
+
+        while (tempNode != null) {
+            System.out.print(tempNode.data + "->");
+            tempNode = tempNode.nextNode;
+        }
+        System.out.println("null");
     }
 }
