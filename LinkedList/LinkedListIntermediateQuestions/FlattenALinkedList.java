9b76e069b15999035e47290c53a56b4d155bc6a6
Pradip <Pradipsahoo7722@gmail.com>
2021-09-15 02:36:16 +0530

    Modify comments

--- LinkedList/LinkedListIntermediateQuestions/FlattenALinkedList.java
+++ LinkedList/LinkedListIntermediateQuestions/FlattenALinkedList.java
@@ -1,10 +1,32 @@
 /**
- * Write a program to flattening a linked list
+ * Write a program to flattening a linked list.
+ * Given a Linked List of size N, where every node represents a sub-linked-list and contains two pointers:
+ * (i) a next pointer to the next node,
+ * (ii) a bottom pointer to a linked list where this node is head.
+ * Each of the sub-linked-list is in sorted order.
+ * Flatten the Link List such that all the nodes appear in a single level while maintaining the sorted order. 
+ * Note: The flattened list will be printed using the bottom pointer instead of next pointer.
+ * 
+ * Example 1:
+ *      Input:  5 -> 10 -> 19 -> 28
+ *              |     |     |     | 
+ *              7     20    22   35
+ *              |           |     | 
+ *              8          50    40
+ *              |                 | 
+ *              30               45
+ *      Output:  5-> 7-> 8- > 10 -> 19-> 20-> 22-> 28-> 30-> 35-> 40-> 45-> 50.
+ * Explanation: The resultant linked lists has every node in a single level.
+ * (Note: | represents the bottom pointer.)
+ * 
+ * Time Complexity : O(N*M)
+ * Space Complexity : O(1)
  */
 package LinkedList.LinkedListIntermediateQuestions;
 
 public class FlattenALinkedList {
 
+    // Node class for creating linked list
     class Node {
 
         int data;
@@ -33,6 +55,7 @@ public class FlattenALinkedList {
         flatten(head);
 
         // Print the flattened and sorted linked list
+        System.out.print("The flatten linked list is : ");
         printList(head);
     }
 
