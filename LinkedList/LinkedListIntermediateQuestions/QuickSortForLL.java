b724ebf3b40a4adb7f1becc1c3e0f99600d7bd9c
Pradip <Pradipsahoo7722@gmail.com>
2021-09-15 17:08:09 +0530

    Add new file

--- LinkedList/LinkedListIntermediateQuestions/QuickSortForLL.java
+++ LinkedList/LinkedListIntermediateQuestions/QuickSortForLL.java
@@ -1,5 +1,21 @@
 /**
- * QuickSort for Linked list.
+ * QuickSort on Linked list.
+ * Write a program to Sort the given Linked List using quicksort.
+ * 
+ * Example 1:
+ *      Input:  N = 5
+ *              value[]  = {3,5,2,4,1}
+ *      Output: 1 2 3 4 5
+ * Explanation: After sorting the given linked list, the resultant will be 1->2->3->4->5.
+ * 
+ * Example 2:
+ *      Input:  N = 3
+ *              value[]  = {9,15,0}
+ *      Output: 0 9 15
+ * Explanation: After sorting the given linked list, resultant will be 0->9->15.
+ * 
+ * Time Complexity : O(nLogn)
+ * Space Complexity : O(n)
  */
 package LinkedList.LinkedListIntermediateQuestions;
 
@@ -11,7 +27,8 @@ public class QuickSortForLL {
     static Node headNode;
     static Node tailNode;
 
-    public class Node {
+    // Node class for creating Linked list
+    class Node {
         int data;
         Node nextNode;
 
@@ -27,11 +44,12 @@ public class QuickSortForLL {
         Scanner sc = new Scanner(System.in);
 
         // Ask user to enter the size of the linked list
-        System.out.println("Enter the length \n of the linked list : ");
+        System.out.print("Enter the length of the linked list : ");
         int length = sc.nextInt();
 
         // Create object of the class
         QuickSortForLL qSortForLL = new QuickSortForLL();
+
         // Ask user to enter the elements of the list
         System.out.println("Enter the elements of the list");
         for (int i = 0; i < length; i++) {
@@ -41,16 +59,36 @@ public class QuickSortForLL {
         }
         sc.close();
 
-        //  Method call to print the general list
+        // Method call to print the general list
         System.out.print("The general List is : ");
         printList();
 
-        //  Method call to sort the list
+        // Method call to sort the list
         sort(headNode, tailNode);
 
-        //  Method call to print the sorted list
+        // Method call to print the sorted list
         System.out.print("The sorted list is : ");
         printList();
+    }
+
+    // Method to create new nodes in the list
+    private static void createNewNode(Node node) {
+
+        // First time when the list is empty then create headNode
+        if (headNode == null) {
+            headNode = node;
+            return;
+        }
+
+        // Make headNode as tempNode
+        tempNode = headNode;
+
+        // Add other nodes in the list
+        while (tempNode.nextNode != null) {
+            tempNode = tempNode.nextNode;
+        }
+        tempNode.nextNode = node;
+        tailNode = tempNode.nextNode;
     }
 
     private static void sort(Node head, Node tail) {
@@ -85,11 +123,11 @@ public class QuickSortForLL {
         int data = pivot.data;
         pivot.data = prev.data;
         prev.data = data;
-        
-        
+
         return prev;
     }
 
+    // Method to print the list
     private static void printList() {
         tempNode = headNode;
 
@@ -99,21 +137,4 @@ public class QuickSortForLL {
         }
         System.out.println();
     }
-
-    private static void createNewNode(Node node) {
-        if (headNode == null) {
-            headNode = node;
-            return;
-        }
-
-        // temp = head
-        tempNode = headNode;
-
-        while (tempNode.nextNode != null) {
-            tempNode = tempNode.nextNode;
-        }
-        tempNode.nextNode = node;
-        tailNode = tempNode.nextNode;
-    }
-
 }
