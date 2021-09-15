8427535da9515cbcfe752c95e78ebc86609e3527
Pradip <Pradipsahoo7722@gmail.com>
2021-09-15 11:53:41 +0530

    Add new file

--- LinkedList/LinkedListIntermediateQuestions/MergeSortForLL.java
+++ LinkedList/LinkedListIntermediateQuestions/MergeSortForLL.java
@@ -1,5 +1,22 @@
 /**
  * Merge Sort For Linked list.
+ * Given Pointer/Reference to the head of the linked list, the task is to Sort the given linked list using Merge Sort.
+ * Note: If the length of linked list is odd, then the extra node should go in the first list while splitting.
+ * 
+ * Example 1:
+ *      Input:  N = 5
+ *              value[]  = {3,5,2,4,1}
+ *      Output: 1 2 3 4 5
+ * Explanation: After sorting the given linked list, the resultant matrix will be 1->2->3->4->5.
+ * 
+ * Example 2:
+ *      Input:  N = 3
+ *              value[]  = {9,15,0}
+ *      Output: 0 9 15
+ * Explanation: After sorting the given linked list, resultant will be 0->9->15.
+ * 
+ * Time Complexity : O(N*Log(N))
+ * Space Complexity : O(N)
  */
 package LinkedList.LinkedListIntermediateQuestions;
 
@@ -10,7 +27,8 @@ public class MergeSortForLL {
     static Node headNode;
     static Node tempNode;
 
-    public class Node {
+    // Node class for creating Linked list
+    class Node {
 
         int data;
         Node nextNode;
@@ -25,14 +43,14 @@ public class MergeSortForLL {
         // Create scanner class object to take user input
         Scanner sc = new Scanner(System.in);
 
-        //  Ask user to enter the length of the linked list
+        // Ask user to enter the length of the linked list
         System.out.println("Enter the length of the linked list : ");
         int length = sc.nextInt();
 
-        //  Create object of the class
+        // Create object of the class
         MergeSortForLL mSortForLL = new MergeSortForLL();
 
-        //  Ask user to enter the elements of the list
+        // Ask user to enter the elements of the list
         System.out.println("Enter the elements of the list : ");
         for (int i = 0; i < length; i++) {
             int val = sc.nextInt();
@@ -41,15 +59,15 @@ public class MergeSortForLL {
         }
         sc.close();
 
-        //  Method call to sort the list
+        // Method call to sort the list
         Node head = mergesort(headNode);
 
-        //  Method call to print the sorted list
+        // Method call to print the sorted list
         System.out.print("The sorted list is : ");
         printList(head);
     }
 
-    //  Method to sort the list
+    // Method to sort the list
     private static Node mergesort(Node head) {
 
         if (head == null || head.nextNode == null) {
@@ -57,11 +75,9 @@ public class MergeSortForLL {
         }
 
         /**
-         * Splitting the list into two halves 
-         * 1. First find the middle of the list.
-         * 2. Then split the list into two halves, head to middle & next of the middle to
-         * last. 
-         * 3. Make next of the middle as null to split the list.
+         * Splitting the list into two halves 1. First find the middle of the list. 2.
+         * Then split the list into two halves, head to middle & next of the middle to
+         * last. 3. Make next of the middle as null to split the list.
          */
         Node middle = getMiddle(head);
         Node nextOfMiddle = middle.nextNode;
@@ -101,12 +117,12 @@ public class MergeSortForLL {
             result = right;
             result.nextNode = merge(left, right.nextNode);
         }
-        
-        //  Returning the resultant list
+
+        // Returning the resultant list
         return result;
     }
 
-    //  Method to find the middle of the list
+    // Method to find the middle of the list
     private static Node getMiddle(Node head) {
 
         if (head == null) {
@@ -120,18 +136,19 @@ public class MergeSortForLL {
         Node fast = head;
 
         /**
-         * first pointer, slow moves 1 node and second pointer, fast moves 2 nodes in one iteration.
+         * first pointer, slow moves 1 node and second pointer, fast moves 2 nodes in
+         * one iteration.
          */
         while (fast.nextNode != null && fast.nextNode.nextNode != null) {
             slow = slow.nextNode;
             fast = fast.nextNode.nextNode;
         }
 
-        //  slow pointer points to the miidle element of the list. so, we return slow
+        // slow pointer points to the miidle element of the list. so, we return slow
         return slow;
     }
 
-    //  Method to print the list
+    // Method to print the list
     private static void printList(Node head) {
         tempNode = head;
 
@@ -142,16 +159,19 @@ public class MergeSortForLL {
         System.out.println();
     }
 
-    //  Method to add new element in the list
+    // Method to creating new nodes in the list
     private static void createNewNode(Node node) {
 
+        // First time when the list is empty then creating headNode
         if (headNode == null) {
             headNode = node;
             return;
         }
 
+        // Make headNode as tempNode
         tempNode = headNode;
 
+        // Add other nodes in the list
         while (tempNode.nextNode != null) {
             tempNode = tempNode.nextNode;
         }
