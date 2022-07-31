8c4e3a068f2aea390a7427a61d6cd61b9ef5b199
Pradip <pradipsahoo7722@gmail.com>
2022-07-31 12:45:02 +0530

    Add new file

---
+++ Love_Babbar_DSA_Sheet/LinkedList/Medium_Questions/Q27_ReverseDLLInGroup.java
@@ -0,0 +1,147 @@
+/**
+ * @author Pradip Sahoo
+ * Created on 31/07/2022
+ * ---------------------------
+ * Reverse a Doubly Linked list in group of Given Size.
+ * Given a doubly linked list containing n nodes. The problem is to reverse
+ * every group of k nodes in the list.
+ * 
+ * Example:
+ *  Input : DLL: 1<->2<->4<->5<->6<->8
+ *          k = 3
+ *  Output : 4<->2<->1<->8<->6<->5
+ * 
+ * Time Complexity : O(N)
+ * Space Complexity : O(1)
+ */
+package Love_Babbar_DSA_Sheet.LinkedList.Medium_Questions;
+
+import java.util.Scanner;
+
+public class Q27_ReverseDLLInGroup {
+
+    static Node headNode;
+    static Node tempNode;
+
+    // Node class for creating new nodes in Linked List
+    class Node {
+
+        int data;
+        Node nextNode;
+        Node prevNode;
+
+        public Node(int data) {
+            this.data = data;
+            nextNode = null;
+            prevNode = null;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the length of the linked list : ");
+        int length = sc.nextInt();
+
+        // Create object of the class
+        Q27_ReverseDLLInGroup rDllInGroup = new Q27_ReverseDLLInGroup();
+
+        // Ask user to enter the elements of the list
+        System.out.println("Enter the elemnts of the list : ");
+        for (int i = 0; i < length; i++) {
+            int val = sc.nextInt();
+            Node hNode = rDllInGroup.new Node(val);
+            createNewNode(hNode);
+        }
+
+        // Ask user to enter the size of the group to reverse DLL
+        System.out.print("Enter the size of group : ");
+        int size = sc.nextInt();
+
+        sc.close();
+
+        // Method call to reverse the list in size
+        headNode = reverseInGroup(headNode, size);
+
+        // Method call to print the list
+        System.out.print("After rotating the array is : ");
+        printList();
+    }
+
+    // Method to create new node in the list
+    private static void createNewNode(Node node) {
+
+        // First time when the list is empty then create the headNode
+        if (headNode == null) {
+            headNode = node;
+            return;
+        }
+
+        // Make headNode as tempNode
+        tempNode = headNode;
+
+        // Add other nodes in the list
+        while (tempNode.nextNode != null) {
+            tempNode = tempNode.nextNode;
+        }
+        tempNode.nextNode = node;
+        node.prevNode = tempNode;
+    }
+
+    /**
+     * Method to reverse a DLL in group
+     * 
+     * @param head
+     * @param size
+     * @return the reverse list
+     */
+    private static Node reverseInGroup(Node head, int size) {
+
+        Node curr = head;
+        Node prev = null;
+        Node next;
+
+        int count = 0;
+
+        while (count < size && curr != null) {
+            next = curr.nextNode;
+            curr.nextNode = prev;
+            curr.prevNode = next;
+            prev = curr;
+            curr = next;
+            count++;
+        }
+
+        /**
+         * Checking if the reversed LinkedList size is equal to K or not. If it is not
+         * equal to k that means we have reversed the last set of size K and we don't
+         * need to call the recursive function
+         */
+        if (count >= size) {
+            Node rest = reverseInGroup(curr, size);
+            head.nextNode = rest;
+            if (rest != null) {
+                rest.prevNode = head;
+            }
+        }
+        return prev;
+    }
+
+    // Method to print the list
+    private static void printList() {
+
+        tempNode = headNode;
+
+        if (headNode == null) {
+            System.out.println("List is empty");
+            return;
+        }
+
+        while (tempNode != null) {
+            System.out.print(tempNode.data + " ");
+            tempNode = tempNode.nextNode;
+        }
+        System.out.println();
+    }
+}
