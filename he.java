0753a0a5542dbea51a8ca52c234e385a5c248048
Pradip <pradipsahoo7722@gmail.com>
2022-09-01 11:48:08 +0530

    Add new file

---
+++ he.java
@@ -0,0 +1,8 @@
+public class he {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 4;
+
+        System.out.println(a + b);
+    }
+}
---
+++ test.java
@@ -0,0 +1,8 @@
+public class test {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 4;
+
+        System.out.println(a + b);
+    }
+}
