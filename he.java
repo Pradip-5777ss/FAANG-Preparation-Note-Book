87d7762124a7612bea7c74ddbbfa107de4192c9e
Pradip <pradipsahoo7722@gmail.com>
2022-09-06 20:37:38 +0530

    Delete files

--- he.java
+++
@@ -1,8 +0,0 @@
-public class he {
-    public static void main(String[] args) {
-        int a = 3;
-        int b = 4;
-
-        System.out.println(a + b);
-    }
-}
--- test.java
+++
@@ -1,8 +0,0 @@
-public class test {
-    public static void main(String[] args) {
-        int a = 3;
-        int b = 4;
-
-        System.out.println(a + b);
-    }
-}
