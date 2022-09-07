0437c0de07a2b8958437fe71451ec5f3cf5ea144
Pradip <pradipsahoo7722@gmail.com>
2022-09-07 15:33:05 +0530

    Add new file

--- Love_Babbar_DSA_Sheet/String/Easy_Questions/Q06_ValidShuffleOfTwoString.java
+++ Love_Babbar_DSA_Sheet/String/Easy_Questions/Q06_ValidShuffleOfTwoString.java
@@ -1,27 +1,36 @@
 /**
+ * @author Pradip Sahoo
+ * Created on 07/09/2022
+ * ----------------------------
  * Write a Program to check whether a string is a valid shuffle of two strings or not.
+ * 
+ * Example:
+ *  Input:  
+ *      str1 = xy
+ *      str2 = 12
+ *      res = x1y2
+ *  Output: true
+ * 
+ * Time Complexity : O(N)
+ * Space Complexity : O(1)
  */
-package String.StringIntermediateQuestions;
+package Love_Babbar_DSA_Sheet.String.Easy_Questions;
 
 import java.util.Scanner;
 
-public class StringValidShuffleOfTwoString {
+public class Q06_ValidShuffleOfTwoString {
 
     public static void main(String[] args) {
 
-        // Create Scanner class object to take user input
         Scanner sc = new Scanner(System.in);
 
-        // Ask user to enter the first string
-        System.out.println("Enter the 1st string : ");
+        System.out.print("Enter the 1st string : ");
         String str1 = sc.nextLine();
 
-        // Ask user to enter the 2nd string
-        System.out.println("Enter the 2nd string : ");
+        System.out.print("Enter the 2nd string : ");
         String str2 = sc.nextLine();
 
-        // Ask user to enter the resultant string to check it is a valid shuffle or not
-        System.out.println("Enter the resultant string : ");
+        System.out.print("Enter the resultant string : ");
         String res = sc.nextLine();
 
         sc.close();
@@ -31,25 +40,25 @@ public class StringValidShuffleOfTwoString {
 
         // print the result
         if (result) {
-            System.out.println(res + " is a valid shuffle of " + str1 + " and " + str2);
+            System.out.println(res + " - is a valid shuffle of " + str1 + " and " + str2);
         } else {
-            System.out.println(res + " is not a valid shuffle of " + str1 + " and " + str2);
+            System.out.println(res + " - is not a valid shuffle of " + str1 + " and " + str2);
         }
     }
 
     // Method to check a string is a valid shuffle of two string or not
     private static boolean isShuffle(String str1, String str2, String res) {
 
-        // First, check the length of all string
-        int length1 = str1.length();
-        int length2 = str2.length();
-        int length3 = res.length();
+        // Find the length of the strings
+        int str1_length = str1.length();
+        int str2_length = str2.length();
+        int res_length = res.length();
 
         /**
          * If the length of first & second string is not equal to the resultant string
          * then return false
          */
-        if (length1 + length2 != length3) {
+        if (str1_length + str2_length != res_length) {
             return false;
         }
 
@@ -57,20 +66,20 @@ public class StringValidShuffleOfTwoString {
         int i = 0, j = 0, k = 0;
 
         // Iterate through all characters of the resultant string
-        while (k != length3) {
+        while (k != res_length) {
 
             /**
-             * check if first character of resultant string matches with first character of
-             * first string
+             * check if ith position character of resultant string matches with ith position
+             * character of first string
              */
-            if (i < length1 && str1.charAt(i) == res.charAt(k)) {
+            if (i < str1_length && str1.charAt(i) == res.charAt(k)) {
                 i++;
             }
             /**
-             * check if first character of resultant string matches with first character of
-             * second string
+             * check if ith position character of resultant string matches with ith position
+             * character of second string
              */
-            else if (j < length2 && str2.charAt(j) == res.charAt(k)) {
+            else if (j < str2_length && str2.charAt(j) == res.charAt(k)) {
                 j++;
             }
             // If the character does not match
@@ -86,10 +95,11 @@ public class StringValidShuffleOfTwoString {
          * After accessing all characters of resultant string if either first or second
          * string has some character left
          */
-        if (i < length1 || j < length2) {
+        if (i < str1_length || j < str2_length) {
             return false;
         }
 
+        // If all characters are matched then return true
         return true;
     }
 }
